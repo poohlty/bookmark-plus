dcb8eb916ad879730862b7965c5003feddc7358b
David Yao <njlxyaoxinwei@gmail.com>
2013-11-09 08:54:10 -0500

    Refactered functions, and added duplicity check

--- menu.js
+++ menu.js
@@ -1,5 +1,41 @@
 
 function genericOnClick(info, tab) {
+
+  function displayNotification(options) {
+      chrome.notifications.create("", options, function(notificationId){
+      console.log('Notification created');
+      window.setTimeout(function(){
+        chrome.notifications.clear(notificationId, function(wasCleared){
+          if (wasCleared) {console.log('Notification cleared');}
+        });
+      }, 2500);
+    });
+  }
+
+  function saveData (dataList, dataEntry) {
+
+    if (dataList) {
+      dataList.push(dataEntry);
+    } else {
+      dataList = [dataEntry];
+    }
+    displayNotification(options);
+    // Save it using the Chrome extension storage API.
+    chrome.storage.sync.set({'test': dataList}, function() {
+    // Notify that we saved.
+    console.log('Settings saved');
+  });
+  }
+
+  function findData (dataList, dataEntry) {
+    for (var i = 0; i < dataList.length; i++) {
+      if (dataEntry.content == dataList[i].content) {
+        return true
+      }
+    }
+    return false
+  }
+
   console.log("This is clicked!");
   console.log("info: " + JSON.stringify(info));
   var link = info.pageUrl;
@@ -29,26 +65,18 @@ function genericOnClick(info, tab) {
 
   var iconUrl = "img/ext-icon-16.png";
   var options = {"type": "basic", "title": "Bookmark Created!", "message": type+" bookmarked!", "iconUrl": iconUrl};
+  var options2 = {"type": "basic", "title": "Bookmark is already There!", "message": "You have already bookmarked this "+type+"!", "iconUrl": iconUrl};
   var dataEntry = {"type": type, "content": content, "link": link, "date": date};
 
   chrome.storage.sync.get('test', function(items){
     var dataList = items.test;
-
-    if (dataList) {
-      dataList.push(dataEntry);
-    } else {
-      dataList = [dataEntry];
+    if (findData(dataList, dataEntry)) {
+      displayNotification(options2);  
     }
-    chrome.notifications.create("", options, function(notificationId){
-      console.log('Notification created');
-      window.setTimeout(function(){
-        chrome.notifications.clear(notificationId, function(wasCleared){
-          if (wasCleared) {console.log('Notification cleared');}
-        });
-      }, 2500);
-    });
-    saveData(dataList);
-    console.log("items" + JSON.stringify(dataList));
+    else {
+      saveData(dataList, dataEntry);
+      console.log("items" + JSON.stringify(dataList));
+    }    
   });
 }
 
@@ -59,13 +87,27 @@ var parent = chrome.contextMenus.create({
   "onclick": genericOnClick
 });
 
-function saveData (value) {
-  // Save it using the Chrome extension storage API.
-  chrome.storage.sync.set({'test': value}, function() {
-    // Notify that we saved.
-    console.log('Settings saved');
-  });
-}
+// function saveData (dataList, dataEntry) {
+
+//     if (dataList) {
+//       dataList.push(dataEntry);
+//     } else {
+//       dataList = [dataEntry];
+//     }
+//     chrome.notifications.create("", options, function(notificationId){
+//       console.log('Notification created');
+//       window.setTimeout(function(){
+//         chrome.notifications.clear(notificationId, function(wasCleared){
+//           if (wasCleared) {console.log('Notification cleared');}
+//         });
+//       }, 2500);
+//     });
+//   // Save it using the Chrome extension storage API.
+//   chrome.storage.sync.set({'test': dataList}, function() {
+//     // Notify that we saved.
+//     console.log('Settings saved');
+//   });
+// }
 
 chrome.browserAction.onClicked.addListener(function(activeTab){
     var newURL = chrome.extension.getURL('bookmark.html');
