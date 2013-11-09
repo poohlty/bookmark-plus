a79d486c12f25e15310bf39c496c2a3d7b250fb9
Tianyu Liu <poohlty@sina.com>
2013-11-09 01:53:03 -0500

    Frontend Rendering class defined

--- bookmark.js
+++ bookmark.js
@@ -1,3 +1,29 @@
+var source = $("#bookmark-template").html();
+var bookmarkTemplate = Handlebars.compile(source);
+
+var Bookmark = Backbone.Model.extend({});
+var BookmarkView = Backbone.View.extend({
+    template: bookmarkTemplate,
+    render: function(){
+        var attributes = this.model.toJSON();
+        this.$el.html(this.template(attributes));
+    },
+    tagName: "div",
+    className: "bookmark"
+});
+
+var bookmarkItem = new Bookmark({
+    type: "type",
+    content: "content",
+    link: "link",
+    date: "date",
+});
+
+var newView = new BookmarkView({
+    model: bookmarkItem
+});
+
+
 document.addEventListener('DOMContentLoaded', function () {
 
     console.log("Here!");
