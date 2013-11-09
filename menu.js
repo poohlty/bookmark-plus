44497d8f455fca1ed800ce79674f501e3191fce5
David Yao <njlxyaoxinwei@gmail.com>
2013-11-09 01:26:30 -0500

    Added type, link, content, date to the bookmarked elements, refined storage

--- menu.js
+++ menu.js
@@ -3,17 +3,58 @@ function genericOnClick(info, tab) {
   console.log("This is clicked!");
   console.log("info: " + JSON.stringify(info));
   console.log("tab: " + JSON.stringify(tab));
+  console.log("Date: " + Date());
+  console.log("pageUrl: " + info.pageUrl);
+  var link = info.pageUrl;
+  var date = Date();
 
-  var entry1 = {"type": 1, "content": 2, "link": 2};
-  var entry2 = {"num": 3, "num2":5};
-  var myList = [entry1, entry2];
-  saveData(myList);
+  var content, type;
+  if (info.selectionText) 
+  {
+    content = info.selectionText;
+    type = "quote"
+  }
+  else if (info.mediaType)
+  {
+    switch (info.mediaType)
+    {
+      case "image":
+        content = info.srcUrl;
+        type = "image";
+        break;
+    }
+
+  }
+  else 
+  {
+     content = link;
+     type = "page"
+
+  }
+
+  var dataEntry = {"type": type, "content": content, "link": link, "date": date};
+  //var entry2 = {"num": 3, "num2":5};
+ // var myList = [entry1, entry2];
+ // saveData(myList);
 
   chrome.storage.sync.get('test', function(items){
     console.log("Get here!" + JSON.stringify(items));
+    var dataList = items.test
+    console.log("items" + JSON.stringify(dataList));
+    if (dataList) 
+    {
+      dataList.push(dataEntry);
+    }
+    else
+    {
+      dataList = [dataEntry];
+    }
+    
+    saveData(dataList);
+
   });
 }
-
+// parent saves the id of the context menu 
 var parent = chrome.contextMenus.create({
   "title": "Bookmark+",
   "contexts": ["all"],
